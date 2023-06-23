58dd1c999e61840ee33c12a2aa8fe1d24cf78a37
joshi <joseph.ilagan.99.com>
2023-06-23 08:40:09 +0800

    part2b

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -3,18 +3,26 @@
 
     class Listing {
         public static function all() {
-            return[
-                [
-                    'id' => 1,
-                    'title' => 'Listing One',
-                    'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident eius officiis dolorum dignissimos officia dolor a. Expedita error illo laborum!'
-                ],    
-                [
-                    'id' => 2,
-                    'title' => 'Listing Two',
-                    'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, officia corrupti. Dolor amet a illo cupiditate dolorem dignissimos quasi in.'
-                ],
-
-            ];
+            return [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. At corrupti alias similique, aperiam molestias saepe sint incidunt illo harum dolorem.'
+            ], 
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. At corrupti alias similique, aperiam molestias saepe sint incidunt illo harum dolorem.'
+            ],
+        ];
+        }
+        public static function find($id) {
+            $listings = self::all();
+            
+            foreach($listings as $listing) {
+                if($listing['id'] == $id) {
+                    return $listing;
+                }
+            }
         }
     }
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,21 @@ use App\Models\Listing;
 |
 */
 
-
+//All listing
 Route::get('/', function () {                       
     return view('listings', [                       
         'heading' => 'Latest Listings',              
         'listings' => Listing::all() 
     ]);                                           
 });    
+
+//Single Listing
+Route::get('/listings/{id}', function ($id) {
+    return view('listing' , [
+    'listing' => Listing::find($id)
+    ]);
+});
+
 
 // Route::get('/hello', function () {
 //     return response ('<h1>Hello Worlds using response helper</h1>', 200)
@@ -42,3 +50,4 @@ Route::get('/', function () {
 //     // dd ($request-> name . ' ' . $request->city);
 //     // return($request-> name . ' ' . $request->city);
 // });
+
